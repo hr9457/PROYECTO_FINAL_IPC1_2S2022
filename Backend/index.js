d1e7b625910feff7b2bec50016090d607e2263b3
hector <hr9457@gmaill.com>
2022-10-13 12:23:02 -0600

    backend terminado

--- Backend/index.js
+++ Backend/index.js
@@ -17,29 +17,86 @@ app.set('port',4000);
 //usando librerias en mi aplicacion
 app.use(morgan('dev')); // para poder visualizar los estados de nuestro servidor
 app.use(express.json()); // para poder manjar los json
-app.use(cors())
+app.use(cors());
 
 
 
+// ENDPOINTS
 
 // logica de su programa
 // get
+// req -> recibe informacion , res -> envia informacion
 app.get('/' , (req, res) => {
 
     // aca dentro
-    console.log('hola mundo');
-    res.send('hola desde nodejs');
+    res.send('<h1>HOLA DESDE EL BACKEND</h1>');
+});
+
+
+app.get('/videos' , (req, res) => {
+
+    var ejemplo = {
+        "videos": "como instalar python",
+        "url": "www.youtube.com"
+    }
+    // aca dentro
+    res.send(ejemplo);
 });
 
 
 //post
-// app.post('/' , (req, res) => {
+app.post('/login' , (req, res) => {
+
+    var dbUsuarios = require('./usuarios.json');
+
+    var usuarioEnviado = req.body.usuario;
+    var passwordEnviado = req.body.password;
+
+    var arregloRepuesta = [];
+
+    // BUSCAR LO QUE YO QUIERO
+    for (var i = 0; i < dbUsuarios.length; i++) {
+        if(dbUsuarios[i].Usuario == usuarioEnviado && dbUsuarios[i].Password == passwordEnviado){            
+            arregloRepuesta.push(dbUsuarios[i]);
+        } 
+    }
+
+
+    // ESE ES EL IF PARA RESPONDERLE AL FRONTEND
+    if (arregloRepuesta.length > 0) {
+        res.send(arregloRepuesta);
+    } else {
+        res.send({Error: "Tu usuario no existe"});
+    }
     
-// });
+});
 
 
 
+//post para buscar jugadores por seleccion
+app.post('/buscador' , (req, res) => {
 
+
+    var nombreSelecion = req.body.Pais;
+
+    var dbJugadores = require('./jugadores.json');
+
+    var arregloRepuesta = [];
+
+    // BUSCAR LO QUE YO QUIERO
+    for (var i = 0; i < dbJugadores.length; i++) {
+        if(dbJugadores[i].Pais == nombreSelecion){
+            arregloRepuesta.push(dbJugadores[i]);
+        }
+    }
+
+    // ESE ES EL IF PARA RESPONDERLE AL FRONTEND
+    if (arregloRepuesta.length > 0) {
+        res.send(arregloRepuesta);
+    } else {
+        res.send({Error: "No existen stickers de esta seleccion"});
+    }
+});
 
 
 
@@ -50,7 +107,8 @@ app.listen(app.get('port'),()=>{
 });
 
 
-
+// comando para arrancar el backend con la libreria nodemon
+// npm run dev
 
 // comando utilizados
 // npm install express
